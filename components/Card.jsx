a97e6e0b4966063dd27ed72f6fbfd8a827834c1a
inveon-mevlutefe <89455717+inveon-mevlutefe@users.noreply.github.com>
2022-05-06 12:51:43 -0400

    Add images

--- components/Card.jsx
+++ components/Card.jsx
@@ -1,43 +1,16 @@
-import { useState } from 'react'
 import styles from '../styles/Card.module.css'
 
-export default function Box({ text, handleClick, isFlip, id }) {
+export default function Box({ card, handleSelect, flipped }) {
 
-  const [card1, setCard1] = useState('card1')
-  const [card2, setCard2] = useState('card2')
-  const [counter, setCounter] = useState(0)
-  const [flipCardClicked, setFlipCardClicked] = useState(false)
-
-  const handleFlip = () => {
-    setFlipCardClicked(true)
-    if (counter == 0) {
-      setCard1(text)
-      setCounter(counter + 1)
-    } else {
-      setCard2(text)
-      setCounter(0)
-      setFlipCardClicked(false)
-    }
-  }
-
-  const checkMatch = () => {
-    if (card1 === card2) {
-      console.log(true)
-    } else {
-      console.log(false)
-    }
+  const handleClick = () => {
+   handleSelect(card)
   }
 
   return (
-    <div className={styles.gridItem} onClick={() => {
-      handleClick(id, text)
-      // handleFlip()
-    }
-      }>
-      <div className={isFlip ? styles.flipCardInner : ''}>
-        <div className={styles.flipCardBack}>
-          {text}
-        </div>
+    <div className={styles.card}>
+      <div className={flipped ? styles.flipped : ''} style={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+        <img className={styles.front} src={card.src} alt={'icon'} style={{display: !flipped ? 'none' : 'block'}}/>
+        <img className={styles.back} src={'/cover.png'} onClick={handleClick}  alt={'cover'} style={{display: flipped ? 'none' : 'block'}}></img>
       </div>
     </div>
   )
--- pages/index.js
+++ pages/index.js
@@ -1,99 +1,78 @@
 import styles from '../styles/Home.module.css'
 import Card from '../components/Card'
-import { useState, useEffect} from 'react'
+import { useState, useEffect } from 'react'
+
+const data = [
+  { src: '/css3.png', matched: false },
+  { src: '/dj.png', matched: false },
+  { src: '/git.png', matched: false },
+  { src: '/html.png', matched: false },
+  { src: '/js-logo.png', matched: false },
+  { src: '/mysql.png', matched: false },
+  { src: '/node.png', matched: false },
+  { src: '/reactt.png', matched: false },
+]
 
 export default function Home() {
-  const [data, setData] = useState([
-    { id: 0, text: 1, flipped: false },
-    { id: 1, text: 1, flipped: false },
-    { id: 2, text: 2, flipped: false },
-    { id: 3, text: 2, flipped: false },
-    { id: 4, text: 3, flipped: false },
-    { id: 5, text: 3, flipped: true },
-    { id: 6, text: 4, flipped: false },
-    { id: 7, text: 4, flipped: false },
-    { id: 8, text: 5, flipped: false },
-    { id: 9, text: 5, flipped: false },
-    { id: 10, text: 6, flipped: false },
-    { id: 11, text: 6, flipped: false },
-    { id: 12, text: 7, flipped: false },
-    { id: 13, text: 7, flipped: false },
-    { id: 14, text: 8, flipped: false },
-    { id: 15, text: 8, flipped: false },
-  ])
-  // const data = [
-  //   { id: 0, text: 1, flipped: false },
-  //   { id: 1, text: 1, flipped: false },
-  //   { id: 2, text: 2, flipped: false },
-  //   { id: 3, text: 2, flipped: false },
-  //   { id: 4, text: 3, flipped: false },
-  //   { id: 5, text: 3, flipped: true },
-  //   { id: 6, text: 4, flipped: false },
-  //   { id: 7, text: 4, flipped: false },
-  //   { id: 8, text: 5, flipped: false },
-  //   { id: 9, text: 5, flipped: false },
-  //   { id: 10, text: 6, flipped: false },
-  //   { id: 11, text: 6, flipped: false },
-  //   { id: 12, text: 7, flipped: false },
-  //   { id: 13, text: 7, flipped: false },
-  //   { id: 14, text: 8, flipped: false },
-  //   { id: 15, text: 8, flipped: false },
-  // ]
 
-  const [card1, setCard1] = useState('card1')
-  const [card2, setCard2] = useState('card2')
-  const [counter, setCounter] = useState(0)
-  const [flipCardClicked, setFlipCardClicked] = useState(false)
+  const [selectedCard1, setSelectedCard1] = useState(null)
+  const [selectedCard2, setSelectedCard2] = useState(null)
 
-  const handleFlip = (id, text) => {
-    console.log(id)
+  const [cards, setCards] = useState([])
+  const [turns, setTurns] = useState(0)
 
-    // setData((prevState) => {
-    //   return [...prevState, {id, text, flip: true}]
-    // })
+  const shuffleData = () => {
+    const shuffledData = [...data, ...data].sort(() => Math.random() - 0.5).map((card) => ({ ...card, id: Math.random() }))
+    setCards(shuffledData)
+    setTurns(0)
+  }
 
-    let temp_data = data
-    let temp_element = temp_data[id]
-    temp_element.flipped = true
-    temp_data[id] = temp_element
-    console.log(temp_data)
-    setData(temp_data)
-    console.log(data)
-
-    // setFlipCardClicked(true)
-
-    // if (counter == 0) {
-    //   setCard1(text)
-    //   setCounter(counter + 1)
-    // } else {
-    //   setCard2(text)
-    //   setCounter(0)
-    //   setFlipCardClicked(false)
-    // }
+  const handleSelect = (item) => {
+    selectedCard1 ? setSelectedCard2(item) : setSelectedCard1(item)
   }
 
   useEffect(() => {
-    console.log('orda')
-  }, [data])
-  
+    if (selectedCard1 && selectedCard2) {
+
+      if (selectedCard1.src === selectedCard2.src) {
+        setCards(prevCards => {
+          return prevCards.map(card => {
+            if(card.src === selectedCard1){
+              return {...card, matched: true}
+            } else {
+              return card
+            }
+          })
+        })
+        resetTurn()
+      } else {
+        console.log('Those cards do not match');
+        setTimeout(() => resetTurn(), 1000)
+      }
+    }
+  }, [selectedCard1, selectedCard2]
+  )
+
+  const resetTurn = () => {
+    setSelectedCard1(null)
+    setSelectedCard2(null)
+    setTurns(prevTurns => prevTurns + 1)
+  }
 
   return (
     <div className={styles.container}>
       <div className={styles.title}>
         <p>Memory Game Layout</p>
-        {console.log('deneme')}
       </div>
       <div className={styles.wrapper}>
         <div className={styles.description}>
-          <button>NEW GAME</button>
+          <button onClick={shuffleData}>NEW GAME</button>
           <p>Score: 100</p>
         </div>
         <div className={styles.gridContainer}>
-          {data
-          // .sort(() => Math.random() - Math.random())
-            .map((item, i) => (
-              <Card key={i} id={i} text={item.text} handleClick={handleFlip} isFlip={item.flipped}/>
-            ))}
+          {cards.map((item) => (
+            <Card key={item.id} card={item} handleSelect={handleSelect} flipped={item === selectedCard1 || item === selectedCard2 || item.matched}/>
+          ))}
         </div>
       </div>
     </div>
